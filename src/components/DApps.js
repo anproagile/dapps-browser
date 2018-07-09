49190ce1718179ec6b368221e0822bebbd1e5eca
anproagile <anproagile@gmail.com>
2018-07-09 14:50:47 -0700

    Do not show daaps

--- src/components/DApps.js
+++ src/components/DApps.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { osName } from 'react-device-detect';
+import { osName, isIOS } from 'react-device-detect';
 import '../App.css';
 import DAppItems from './DAppItems';
 import DAppTopCards from './DAppTopCards';
 import { TrustClient } from '../network/TrustClient';
-import { TrustWeb3 } from "../network/TrustWeb3";
-
+import getWeb3 from '../utils/provider';
+import DAppsDisabled from './DAppsDisabled';
+import { getTrsutBrowserVersion } from "../components/systemchecks/BrowserVersion";
 class DApps extends React.Component {
   constructor(props) {
     super(props);
     this.state = { data: [] };
     this.trustClient = new TrustClient();
-    this.trustWeb3 = new TrustWeb3();
   }
 
   componentWillMount() {
     this.fetch();
   }
 
-  async fetch() {
-    try {
-      const networkId = await this.trustWeb3.getNetwork();
-      const bootstrap = await this.trustClient.fetchBootstrap(networkId, osName);
-      this.setState({ data: bootstrap.data.docs });
-    } catch (error) {
-      console.log(`Error at fetch()`, error)
-    }
+  fetch() {
+    const network = parseInt(getWeb3().version.network, 10);
+    this.trustClient.fetchBootstrap(network, osName).then((response) => {
+      this.setState({ data: response.data.docs });
+    });
   }
 
   render() {
+    const browserVersion = getTrsutBrowserVersion()
+    if (browserVersion >= 1.68 && isIOS) {
+      return (
+        <DAppsDisabled />
+      )
+    }
     const elements = this.state.data || [];
     const categoryID = '5abcceb4682db901241a0636';
     const newDApp = elements.filter((item) => {
